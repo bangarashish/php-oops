9ed7fc07d927724c9bb65eea9ec5c0a70f01258e
ashwini1966 <bhawalkarashwini1996@gmail.com>
2023-03-14 18:55:15 +0530

    changes saved

--- admin/common/config.php
+++ admin/common/config.php
@@ -11,3 +11,30 @@
     }
    
 ?>
+
+        <?php 
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $dbname = "unspoken";
+
+        // // Create connection
+        // $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // // Check connection
+        // if (mysqli_connect_errno())
+        // {
+        // echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        // }
+
+        // $base_url = "http://localhost/amanda/admin/";
+
+        // if (!defined('base_url')){
+        // define("base_url", "http://localhost/amanda/admin/");
+        // }
+
+        // if (!function_exists('base_url')) {
+        // function base_url($url){
+        //     return base_url.$url;
+        // }
+        // } 
+        ?>
---
+++ admin/newIndex.php
@@ -0,0 +1,69 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+// echo $request;
+switch ($request) {
+    case '/amanda/admin/' :
+        require __DIR__ . '/modules/authentication/login.php';
+        break;
+    case '/amanda/admin/index' :
+        require __DIR__ . '/modules/authentication/login.php';
+        break;
+    case '/amanda/admin/forget_password' :
+        require __DIR__ . '/modules/authentication/forget_password.php';
+        break;
+    case '/amanda/admin/logout' :
+        require __DIR__ . '/modules/authentication/logout.php';
+        break;
+    case '/amanda/admin/dashboard' :
+        require __DIR__ . '/modules/main/dashboard.php';
+        break;
+    case '/amanda/admin/clients' :
+        require __DIR__ . '/modules/users/manage_clients.php';
+        break;
+    case '/amanda/admin/interpreters' :
+        require __DIR__ . '/modules/users/manage_interpreter.php';
+        break;
+    case '/amanda/admin/jobs' :
+        require __DIR__ . '/modules/jobs/manage_jobs.php';
+        break;
+    case '/amanda/admin/job_assignment' :
+        require __DIR__ . '/modules/jobs/job_assignment.php';
+        break;
+    case '/amanda/admin/job_schedule' :
+        require __DIR__ . '/modules/jobs/job_schedule.php';
+        break;
+    case '/amanda/admin/editJob' :
+        require __DIR__ . '/modules/jobs/edit_job.php';
+        break;
+    case '/amanda/admin/jobDetails' :
+        require __DIR__ . '/modules/jobs/job_details.php';
+        break;
+    case '/amanda/admin/manage_sms' :
+        require __DIR__ . '/modules/smsTemplate/manage_sms.php';
+        break;
+    case '/amanda/admin/addSmsTemplate' :
+        require __DIR__ . '/modules/smsTemplate/add_sms_template.php';
+        break;
+    case '/amanda/admin/editSmsTemplate' :
+        require __DIR__ . '/modules/smsTemplate/edit_sms_template.php';
+        break;
+    case '/amanda/admin/manage_email' :
+        require __DIR__ . '/modules/emailTemplate/manage_email.php';
+        break;
+    case '/amanda/admin/addEmailTemplate' :
+        require __DIR__ . '/modules/emailTemplate/add_email_template.php';
+        break;
+    case '/amanda/admin/editEmailTemplate' :
+        require __DIR__ . '/modules/emailTemplate/edit_email_template.php';
+        break;
+    case '/amanda/admin/settings' :
+        require __DIR__ . '/modules/main/settings.php';
+        break;
+    default:
+        http_response_code(404);
+        // require __DIR__ . '/views/404.php';
+        break;
+}
+
+?>
